0085eb2498c688b1de8a8fb1fc9ec3270b89428a
Cheese Allergy Hamster <shlifedev@gmail.com>
2022-07-15 07:09:26 +0900

    concrete to flexible function

--- concreable.cs
+++ concreable.cs
@@ -1,9 +1,8 @@
 
-//concreteable function sample
-//require
 Func<(int, IntPtr)> Concrete(Func<IntPtr> filter, IntPtr @event, int deap, int level)
 {
-    var queues = EOM.Call(flower, "objects.all(obj=>{obj.force(&event)})", deap, level);
+    // 32비트에서는 ToInt64->32 분기
+    var queues = EOM.Call(flower, $"objects.all(obj=>{obj.force(&event)}).send({@event.ToInt64()})", deap, level);
     var complatedStream = EOM.StreamOfModel(queues, -1);
     var streamModelSizePtr = complatedStream.ToPtr();
     IntPtr concretedModelPtr = filter(streamModelSizePtr); 
